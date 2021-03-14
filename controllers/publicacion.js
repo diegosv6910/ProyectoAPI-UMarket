a76536dc454ef88c0316e025ff18a918b78acab2
Diego Sandoval <diegosv6910@gmail.com>
2021-03-14 09:21:14 -0600

    CRUD publicaciones CHECK ALL

--- controllers/publicacion.js
+++ controllers/publicacion.js
@@ -1,41 +1,221 @@
-/*  Archivo controllers/publicaciones.js
- *  Simulando la respuesta de objetos publicacion
- *  en un futuro aquí se utilizarán los modelos
- */
+var sqlDetails = require('../database')
+var mysql = require('mysql')
 
-// importamos el modelo de usuario
-const Publicacion = require('../models/Publicaciones')
-
-function crearPublicacion(req, res) {
-  // Instanciaremos un nuevo usuario utilizando la clase usuario
-  var publicacion = new Publicacion(req.body)
-  res.status(201).send(publicacion)
+//CREATE
+//Funcion para registrar una Publicacion
+function crearPublicacionesUsuario(req, res) {
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Creamos query con la consulta
+  var sql = "INSERT INTO `bsn7gx0xxd03i3hgfmyr`.`publicaciones` (`fechaPublicacion`, `creadorUPublicacion`, `Status`) VALUES ('" + req.body.fechaPublicacion + "', '" + req.body.creadorUPublicacion + "', '" + req.body.Status + "');";
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if(err) throw err;
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
 }
 
-function obtenerPublicacion(req, res) {
-  // Simulando dos usuarios y respondiendolos
-  var publicacion1 = new Publicacion(1, '28/02/2021', 'Diego', 'Activa');
-  var publicacion2 = new Publicacion(2, '28/02/2021', 'Daniel', 'Terminada');
-  res.send([publicacion1, publicacion2])
+//CREATE
+//Funcion para registrar una Publicacion
+function crearPublicacionesDistribuidor(req, res) {
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Creamos query con la consulta
+  var sql = "INSERT INTO `bsn7gx0xxd03i3hgfmyr`.`publicaciones` (`fechaPublicacion`, `creadorDPublicacion`, `Status`) VALUES ('" + req.body.fechaPublicacion + "', '" + req.body.creadorDPublicacion + "', '" + req.body.Status + "');";
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if(err) throw err;
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
 }
 
-function modificarPublicacion(req, res) {
-  // simulando un usuario previamente existente que el cliente modifica
-  var publicacion1 = new Publicacion(req.params.id, '28/02/2021', 'Diego', 'Terminada')
-  var modificaciones = req.body;
-  publicacion1 = { ...publicacion1, ...modificaciones }
-  res.send(publicacion1)
+// return res.status(201).json(user.toAuthJSON())
+
+//READ
+//Funcion para obtener todos los Publicaciones
+function obtenerPublicaciones(req, res) {
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if (err) throw err;
+    //Creamos query con la consulta
+    var sql = "SELECT * FROM publicaciones";
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
 }
 
-function eliminarPublicacion(req, res) {
-  // se simula una eliminación de usuario, regresando un 200
-  res.status(200).send(`Usuario ${req.params.id} eliminado`);
+//READ
+//Funcion para obtener 1 Publicacion de acuerdo a su ID
+function obtenerSimplePublicaciones(req, res) {
+  //Obtengo el ID enviado como Parametro
+  idBusqueda = req.params.id;
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if (err) throw err;
+    //Creamos query con la consulta
+    var sql = "SELECT * FROM publicaciones where idPublicacioneses = '"+idBusqueda+"'";
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
+}
+
+//READ
+//Funcion para obtener solo un campo de un Publicacion
+function obtenerParametroPublicaciones(req, res) {
+  //Obtengo el ID enviado como Parametro
+  parametro = req.params.parametro;
+  console.log(parametro)
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if (err) throw err;
+    //Creamos query con la consulta
+    var sql = "SELECT "+parametro+" from bsn7gx0xxd03i3hgfmyr.publicaciones";
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
+}
+
+//READ
+//Funcion para obtener Publicaciones que compartan un mismo atributo.
+function obtenerAtributoPublicaciones(req, res) {
+  //Obtengo el ID enviado como Parametro
+  parametro = req.params.parametro;
+  console.log(parametro)
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if (err) throw err;
+    //Creamos query con la consulta
+    var sql = "SELECT "+parametro+", COUNT("+parametro+") FROM publicaciones GROUP BY "+parametro+" HAVING COUNT('"+parametro+"') > 1;"
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
+}
+
+//READ
+//Funcion para obtener Publicacion pero con un limite de muestras
+function obtenerLimitePublicacioneses(req, res) {
+  //Obtengo el ID enviado como Parametro
+  limite = req.params.limite;
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if (err) throw err;
+    //Creamos query con la consulta
+    var sql = "SELECT * from bsn7gx0xxd03i3hgfmyr.publicaciones limit "+limite+"";
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
+}
+
+//UPDATE
+//Funcion para actualizar la informacion de un Publicacion. (CAMPO UNICO).
+//El campo que se modifica es el Status
+function modificaAtributoPublicaciones(req, res) {
+  //Almacenamos en una variable ID el ID que se envia como parametro.
+  var idBusqueda = req.params.id;
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if (err) throw err;
+    //Creamos query con la consulta
+    var sql = "UPDATE `bsn7gx0xxd03i3hgfmyr`.`publicaciones` SET `status` = '"+req.body.Status+"' WHERE (`idPublicaciones` = '"+idBusqueda+"');"
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
+}
+
+//DELETE
+//Funcion para eliminar un Publicacion
+function eliminarPublicaciones(req, res) {
+  //Obtengo el ID enviado como Parametro
+  idBusqueda = req.params.id;
+  //Creamos la variable con que es una conexion a mysql
+  con = mysql.createConnection(sqlDetails);
+  //Nos conectamos a la base de datos
+  con.connect(function (err) {
+    //Verificamos que no existan errores
+    if (err) throw err;
+    //Creamos query con la consulta
+    var sql = "DELETE FROM `bsn7gx0xxd03i3hgfmyr`.`publicaciones` WHERE (`idPublicaciones` = '"+idBusqueda+"');";
+    //Ejecutamos el query con la conexion creada
+    con.query(sql, function (err, result) {
+      //Verificamos que no existan errores
+      if(err) throw err;
+      //Retornamos un JSON con la informacion creada y terminamos la conexion
+      return res.json(result), con.end();
+    });
+  });
 }
 
 // exportamos las funciones definidas
 module.exports = {
-  crearPublicacion,
-  obtenerPublicacion,
-  modificarPublicacion,
-  eliminarPublicacion
+  crearPublicacionesUsuario,
+  crearPublicacionesDistribuidor,
+  obtenerPublicaciones,
+  obtenerSimplePublicaciones,
+  obtenerLimitePublicacioneses,
+  obtenerParametroPublicaciones,
+  obtenerAtributoPublicaciones,
+  modificaAtributoPublicaciones,
+  eliminarPublicaciones
 }
--- routes/publicaciones.js
+++ routes/publicaciones.js
@@ -1,15 +1,33 @@
 // Estructura del CRUD
 const router = require('express').Router();
 const {
-  crearPublicacion,
-  obtenerPublicacion,
-  modificarPublicacion,
-  eliminarPublicacion
+  obtenerPublicaciones,
+  obtenerSimplePublicaciones,
+  obtenerLimitePublicacioneses,
+  obtenerParametroPublicaciones,
+  obtenerAtributoPublicaciones,
+  modificaAtributoPublicaciones,
+  eliminarPublicaciones,
+  crearPublicacionesDistribuidor,
+  crearPublicacionesUsuario
 } = require('../controllers/publicacion')
 
-router.get('/', obtenerPublicacion)
-router.post('/', crearPublicacion)
-router.put('/:id', modificarPublicacion)
-router.delete('/:id', eliminarPublicacion)
-
+//Ruta para obtener todos los Publicacioneses
+router.get('/', obtenerPublicaciones)
+//Ruta para obtener un solo Publicaciones identificado por su ID
+router.get('/:id', obtenerSimplePublicaciones)
+//Ruta para obtener un numero determinado de Publicacioneses
+router.get('/consulta/:limite', obtenerLimitePublicacioneses)
+//Ruta para obtener un solo campo de los Publicacioneses
+router.get('/parametro/:parametro', obtenerParametroPublicaciones)
+//Ruta para obtener un solo campo de los Publicacioneses
+router.get('/atributo/:parametro', obtenerAtributoPublicaciones)
+//Ruta para crear un Publicaciones como Usuario
+router.post('/usuario', crearPublicacionesUsuario)
+//Ruta para crear un Publicaciones como Distribuidor
+router.post('/distribuidor', crearPublicacionesDistribuidor)
+//Ruta para modificar el unico atributo modificable de un Publicaciones (STATUS) (Identificado por un ID)
+router.put('/:id', modificaAtributoPublicaciones)
+// Ruta para eliminar un Publicaciones
+router.delete('/:id',eliminarPublicaciones)
 module.exports = router;
