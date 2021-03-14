e1075d45a2ea334c1d8b54e568ed5d2ddfcc40fe
Diego Sandoval <diegosv6910@gmail.com>
2021-03-13 19:30:00 -0600

    CRUD distribuidor CHECK ALL

--- controllers/distribuidor.js
+++ controllers/distribuidor.js
@@ -44,12 +44,12 @@ var sqlDetails = require('../database')
 var mysql = require('mysql')
 
 //CREATE
-//Funcion para registrar un administrador
-function crearAdministrador(req, res) {
+//Funcion para registrar un Distribuidor
+function crearDistribuidor(req, res) {
   //Creamos la variable con que es una conexion a mysql
   con = mysql.createConnection(sqlDetails);
   //Creamos query con la consulta
-  var sql = "INSERT INTO `bsn7gx0xxd03i3hgfmyr`.`administrador` (`nombreAdministrador`, `direccionAdministrador`, `telefonoAdministrador`, `correoAdministrador`, `urlFoto`) VALUES ('" + req.body.nombreAdministrador + "', '" + req.body.direccionAdministrador + "', '" + req.body.telefonoAdministrador + "', '" + req.body.correoAdministrador + "','" + req.body.urlFoto + "');";
+  var sql = "INSERT INTO `bsn7gx0xxd03i3hgfmyr`.`distribuidor` (`nombreDistribuidor`, `domicilioDistribuidor`, `telefonoDistribuidor`, `correoDistribuidor`, `identificacionDistribuidor`, `ramo`) VALUES ('" + req.body.nombreDistribuidor + "', '" + req.body.direccionDistribuidor + "', '" + req.body.telefonoDistribuidor + "', '" + req.body.correoDistribuidor + "','" + req.body.urlFoto + "', '"+ req.body.ramo +"');";
   //Nos conectamos a la base de datos
   con.connect(function (err) {
     //Verificamos que no existan errores
@@ -67,8 +67,8 @@ function crearAdministrador(req, res) {
 // return res.status(201).json(user.toAuthJSON())
 
 //READ
-//Funcion para obtener todos los administradores
-function obtenerAdministrador(req, res) {
+//Funcion para obtener todos los Distribuidores
+function obtenerDistribuidor(req, res) {
   //Creamos la variable con que es una conexion a mysql
   con = mysql.createConnection(sqlDetails);
   //Nos conectamos a la base de datos
@@ -76,7 +76,7 @@ function obtenerAdministrador(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "SELECT * FROM administrador";
+    var sql = "SELECT * FROM distribuidor";
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -88,8 +88,8 @@ function obtenerAdministrador(req, res) {
 }
 
 //READ
-//Funcion para obtener 1 Administrador de acuerdo a su ID
-function obtenerSimpleAdministrador(req, res) {
+//Funcion para obtener 1 Distribuidor de acuerdo a su ID
+function obtenerSimpleDistribuidor(req, res) {
   //Obtengo el ID enviado como Parametro
   idBusqueda = req.params.id;
   //Creamos la variable con que es una conexion a mysql
@@ -99,7 +99,7 @@ function obtenerSimpleAdministrador(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "SELECT * FROM administrador where idAdministrador = '"+idBusqueda+"'";
+    var sql = "SELECT * FROM distribuidor where idDistribuidor = '"+idBusqueda+"'";
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -111,8 +111,8 @@ function obtenerSimpleAdministrador(req, res) {
 }
 
 //READ
-//Funcion para obtener solo un campo de un administrador
-function obtenerParametroAdministrador(req, res) {
+//Funcion para obtener solo un campo de un Distribuidor
+function obtenerParametroDistribuidor(req, res) {
   //Obtengo el ID enviado como Parametro
   parametro = req.params.parametro;
   console.log(parametro)
@@ -123,7 +123,7 @@ function obtenerParametroAdministrador(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "SELECT "+parametro+" from bsn7gx0xxd03i3hgfmyr.administrador";
+    var sql = "SELECT "+parametro+" from bsn7gx0xxd03i3hgfmyr.distribuidor";
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -135,8 +135,8 @@ function obtenerParametroAdministrador(req, res) {
 }
 
 //READ
-//Funcion para obtener Administradores que compartan un mismo atributo.
-function obtenerAtributoAdministrador(req, res) {
+//Funcion para obtener Distribuidores que compartan un mismo atributo.
+function obtenerAtributoDistribuidor(req, res) {
   //Obtengo el ID enviado como Parametro
   parametro = req.params.parametro;
   console.log(parametro)
@@ -147,7 +147,7 @@ function obtenerAtributoAdministrador(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "SELECT "+parametro+", COUNT("+parametro+") FROM administrador GROUP BY "+parametro+" HAVING COUNT('"+parametro+"') > 1;"
+    var sql = "SELECT "+parametro+", COUNT("+parametro+") FROM distribuidor GROUP BY "+parametro+" HAVING COUNT('"+parametro+"') > 1;"
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -159,8 +159,8 @@ function obtenerAtributoAdministrador(req, res) {
 }
 
 //READ
-//Funcion para obtener administrador pero con un limite de muestras
-function obtenerLimiteAdministradores(req, res) {
+//Funcion para obtener Distribuidor pero con un limite de muestras
+function obtenerLimiteDistribuidores(req, res) {
   //Obtengo el ID enviado como Parametro
   limite = req.params.limite;
   //Creamos la variable con que es una conexion a mysql
@@ -170,7 +170,7 @@ function obtenerLimiteAdministradores(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "SELECT * from bsn7gx0xxd03i3hgfmyr.administrador limit "+limite+"";
+    var sql = "SELECT * from bsn7gx0xxd03i3hgfmyr.distribuidor limit "+limite+"";
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -182,9 +182,9 @@ function obtenerLimiteAdministradores(req, res) {
 }
 
 //UPDATE
-//Funcion para actualizar la informacion de un administrador. (CAMPO UNICO).
+//Funcion para actualizar la informacion de un Distribuidor. (CAMPO UNICO).
 //El campo que se modifica es el nombre
-function modificaAtributoAdministrador(req, res) {
+function modificaAtributoDistribuidor(req, res) {
   //Almacenamos en una variable ID el ID que se envia como parametro.
   var idBusqueda = req.params.id;
   //Creamos la variable con que es una conexion a mysql
@@ -194,7 +194,7 @@ function modificaAtributoAdministrador(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "UPDATE `bsn7gx0xxd03i3hgfmyr`.`administrador` SET `nombreAdministrador` = '"+req.body.nombreAdministrador+"' WHERE (`idAdministrador` = '"+idBusqueda+"');"
+    var sql = "UPDATE `bsn7gx0xxd03i3hgfmyr`.`distribuidor` SET `nombreDistribuidor` = '"+req.body.nombreDistribuidor+"' WHERE (`idDistribuidor` = '"+idBusqueda+"');"
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -206,10 +206,10 @@ function modificaAtributoAdministrador(req, res) {
 }
 
 //UPDATE
-//Funcion para actualizar la informacion de un administrador. (TODOS LOS CAMPOS).
+//Funcion para actualizar la informacion de un Distribuidor. (TODOS LOS CAMPOS).
 //Se modifican todos los campos salvo el ID.
 //Recibe un JSON con la informacion a modificar
-function modificaAtributosAdministrador(req, res) {
+function modificaAtributosDistribuidor(req, res) {
   //Almacenamos en una variable ID el ID que se envia como parametro.
   var idBusqueda = req.body.idAdmin;
   //Creamos la variable con que es una conexion a mysql
@@ -219,7 +219,7 @@ function modificaAtributosAdministrador(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "UPDATE `bsn7gx0xxd03i3hgfmyr`.`administrador` SET `nombreAdministrador` = '"+req.body.nombreAdministrador+"', `direccionAdministrador` = '"+req.body.direccionAdministrador+"', `telefonoAdministrador` = '"+req.body.telefonoAdministrador+"', `correoAdministrador` = '"+req.body.correoAdministrador+"', `urlFoto` = '"+req.body.urlFoto+"' WHERE (`idAdministrador` = '"+req.body.idAdmin+"');"
+    var sql = "UPDATE `bsn7gx0xxd03i3hgfmyr`.`distribuidor` SET `nombreDistribuidor` = '"+req.body.nombreDistribuidor+"', `domicilioDistribuidor` = '"+req.body.direccionDistribuidor+"', `telefonoDistribuidor` = '"+req.body.telefonoDistribuidor+"', `correoDistribuidor` = '"+req.body.correoDistribuidor+"', `identificacionDistribuidor` = '"+req.body.urlFoto+"', `ramo` = '"+req.body.ramo+"' WHERE (`idDistribuidor` = '"+req.body.idAdmin+"');"
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -231,8 +231,8 @@ function modificaAtributosAdministrador(req, res) {
 }
 
 //DELETE
-//Funcion para eliminar un administrador
-function eliminarAdministrador(req, res) {
+//Funcion para eliminar un Distribuidor
+function eliminarDistribuidor(req, res) {
   //Obtengo el ID enviado como Parametro
   idBusqueda = req.params.id;
   //Creamos la variable con que es una conexion a mysql
@@ -242,7 +242,7 @@ function eliminarAdministrador(req, res) {
     //Verificamos que no existan errores
     if (err) throw err;
     //Creamos query con la consulta
-    var sql = "DELETE FROM `bsn7gx0xxd03i3hgfmyr`.`administrador` WHERE (`idAdministrador` = '"+idBusqueda+"');";
+    var sql = "DELETE FROM `bsn7gx0xxd03i3hgfmyr`.`distribuidor` WHERE (`idDistribuidor` = '"+idBusqueda+"');";
     //Ejecutamos el query con la conexion creada
     con.query(sql, function (err, result) {
       //Verificamos que no existan errores
@@ -255,13 +255,13 @@ function eliminarAdministrador(req, res) {
 
 // exportamos las funciones definidas
 module.exports = {
-  crearAdministrador,
-  obtenerAdministrador,
-  obtenerSimpleAdministrador,
-  obtenerLimiteAdministradores,
-  obtenerParametroAdministrador,
-  obtenerAtributoAdministrador,
-  modificaAtributoAdministrador,
-  modificaAtributosAdministrador,
-  eliminarAdministrador
+  crearDistribuidor,
+  obtenerDistribuidor,
+  obtenerSimpleDistribuidor,
+  obtenerLimiteDistribuidores,
+  obtenerParametroDistribuidor,
+  obtenerAtributoDistribuidor,
+  modificaAtributoDistribuidor,
+  modificaAtributosDistribuidor,
+  eliminarDistribuidor
 }
--- routes/distribuidores.js
+++ routes/distribuidores.js
@@ -3,13 +3,31 @@ const router = require('express').Router();
 const {
   crearDistribuidor,
   obtenerDistribuidor,
-  modificarDistribuidor,
+  obtenerSimpleDistribuidor,
+  obtenerLimiteDistribuidores,
+  obtenerParametroDistribuidor,
+  obtenerAtributoDistribuidor,
+  modificaAtributoDistribuidor,
+  modificaAtributosDistribuidor,
   eliminarDistribuidor
-} = require('../controllers/distribuidor')
+} = require('../controllers/Distribuidor')
 
+//Ruta para obtener todos los Distribuidores
 router.get('/', obtenerDistribuidor)
+//Ruta para obtener un solo Distribuidor identificado por su ID
+router.get('/:id', obtenerSimpleDistribuidor)
+//Ruta para obtener un numero determinado de Distribuidores
+router.get('/consulta/:limite', obtenerLimiteDistribuidores)
+//Ruta para obtener un solo campo de los Distribuidores
+router.get('/parametro/:parametro', obtenerParametroDistribuidor)
+//Ruta para obtener un solo campo de los Distribuidores
+router.get('/atributo/:parametro', obtenerAtributoDistribuidor)
+//Ruta para crear un Distribuidor
 router.post('/', crearDistribuidor)
-router.put('/:id', modificarDistribuidor)
-router.delete('/:id', eliminarDistribuidor)
-
+//Ruta para modificar un unico atributo de un Distribuidor (Identificado por un ID)
+router.put('/:id', modificaAtributoDistribuidor)
+//Ruta para modificar todos los atributos de un Distribuidor. (Identificado por un ID)
+router.put('/', modificaAtributosDistribuidor)
+// Ruta para eliminar un Distribuidor
+router.delete('/:id',eliminarDistribuidor)
 module.exports = router;
